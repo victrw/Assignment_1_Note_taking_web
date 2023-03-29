f41deafa81ec284e010addced022eca70b36d577
Victor <victrwng@gmail.com>
2023-03-29 12:30:05 -0700

    Changed #theme into class, butn color to white

--- jscript/javascript.js
+++ jscript/javascript.js
@@ -1,14 +1,22 @@
-const darkthemebutton = document.querySelector("#theme")
+const darkthemebutton = document.querySelector(".theme")
+const darksidebar = document.querySelector('.my-notes-container')
+darkthemebutton.textContent = "Dark Theme"
 
 
 function darktheme(e) {
     console.log(e.target)   
     if (e.target.tagName === "BUTTON") {
-        document.body.classList.add("darkBG")
-        const darksidebar = document.querySelector('.my-notes-container')
-        darksidebar.classList.add('sidebardarkBG')
+        document.body.classList.toggle("darkBG")
+        darksidebar.classList.toggle('sidebardarkBG')
+        if (darkthemebutton.textContent === "Dark Theme") {
+            darkthemebutton.textContent = "Light Theme";
+            darkthemebutton.classList.add("lightthemebutton") 
+        } else {
+            darkthemebutton.textContent = "Dark Theme";
+            darkthemebutton.classList.remove("lightthemebutton")
+        }
     }
 }
 
-darkthemebutton.addEventListener("click", darktheme)
 
+darkthemebutton.addEventListener("click", darktheme)
