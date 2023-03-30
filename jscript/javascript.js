2ba4c5fb53548359c4df20c7f2ecd1e7e3266061
Victor <victrwng@gmail.com>
2023-03-29 18:31:06 -0700

    Added cancel button to remove txtarea and buttons

--- jscript/javascript.js
+++ jscript/javascript.js
@@ -1,11 +1,12 @@
 const darkthemebutton = document.querySelector(".theme")
 const darksidebar = document.querySelector('.my-notes-container')
 const txtarea = document.querySelector("textarea")
+const cancel_button = document.querySelector(".cancel")
+const save_button = document.querySelector("#save")
 darkthemebutton.textContent = "Dark Theme"
 
 
 function darktheme(e) {
-    console.log(e.target)   
     if (e.target.tagName === "BUTTON") {
         document.body.classList.toggle("darkBG")
         darksidebar.classList.toggle('sidebardarkBG')
@@ -23,3 +24,15 @@ function darktheme(e) {
 
 
 darkthemebutton.addEventListener("click", darktheme)
+
+
+function hidecontent(e) {
+    if (e.target.tagName === "BUTTON") {
+        console.log(e)
+        cancel_button.classList.add("hidecontent");
+        txtarea.classList.add("hidecontent");
+        save_button.classList.add("hidecontent")
+    }
+}
+
+cancel_button.addEventListener("click", hidecontent)
